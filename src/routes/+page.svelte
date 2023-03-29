dbd5324fabd64e2c03acfff5847808518da00b76
Simon Orvarsson <simon.orvarsson@gmail.com>
2023-03-29 22:24:56 +0200

    added logo and contact info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,30 @@
-<h1>Welcome to Orvify</h1>
+<script>
+	import { Fullpage, FullpageSection } from 'svelte-fullpage';
+</script>
+
+<Fullpage>
+	<FullpageSection>
+		<div>
+			<h1>
+				Orvify AB <a href="https://www.bolagsfakta.se/5594215252-Orvify_AB">(559421-5252)</a>
+			</h1>
+		</div>
+	</FullpageSection>
+	<FullpageSection>
+		<div>
+			<h2><a href="mailto:simon@orvify.se">simon@orvify.se</a></h2>
+		</div></FullpageSection
+	>
+	<FullpageSection>
+		<div>
+			<img src="/orvify-logo.svg" alt="Orvify AB logo" />
+		</div></FullpageSection
+	>
+</Fullpage>
 
 <style>
-	body,
-	h1 {
+	:global(body) {
+		height: 100vh;
 		font-family: helvetica, arial, sans-serif;
 	}
 </style>
