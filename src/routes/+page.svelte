a30af0d4fe0bf2be5e5a0e35c2ebb69a82ef1c63
Simon Orvarsson <simon.orvarsson@gmail.com>
2024-02-21 21:14:42 +0100

    npm 18

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+
+<svelte:head>
+  <title>Orvify - freelance frontenders</title>
+  <meta name="description" content="Orvify - freelance frontenders with great experience in modern javascript frameworks such as React, Vue, Svelte and Open UI5/Fiori" />
+</svelte:head>
+
+
 <h1>Orvify AB (559421-5252)</h1>
 <p><a href="mailto:simon@orvify.se">simon@orvify.se</a></p>
+<img src="/orvify-logo.svg" alt="Orvify AB" />
